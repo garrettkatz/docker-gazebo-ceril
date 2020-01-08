dc39db5f90a11f5dbb94339a711c8bb8791871f7
garrettkatz <garrett.katz@gmail.com>
2020-01-08 16:39:48 -0500

    clarifying comment

--- src/startup.sh
+++ src/startup.sh
@@ -3,7 +3,7 @@
 # setup new user with same uid and gid as host for X server access
 # environment variables GUI_UID and GUI_GID will be set by host in run.sh
 usermod --uid $GUI_UID $USERNAME
-sudo groupmod --gid $GUI_GID $USERNAME
+sudo groupmod --gid $GUI_GID $USERNAME # why Dockerfile installed sudo
 
 # Enter bash shell
 /bin/bash

e2035bd5ef51a28a1628266e54aafed3f3178630
amulyasaibellam <ammu2810@gmail.com>
2020-01-08 07:47:33 -0500

    removed poppy-ws

--- poppy-ws/build/poppy-control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/amul/poppy-ws/src/poppy-control
-# Build directory: /home/amul/poppy-ws/build/poppy-control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
